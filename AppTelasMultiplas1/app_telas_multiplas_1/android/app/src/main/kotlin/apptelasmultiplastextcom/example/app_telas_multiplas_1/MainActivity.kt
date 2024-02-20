33090a4bc8abb0680a116ee3dffe82cd05fb7de0
Vinicius Cavalieri <125322132+ViniBOYs@users.noreply.github.com>
2024-02-20 19:49:08 -0300

    AppMultiplasTelas
    Criação do primeiro app de multiplas telas. push 1°

---
+++ AppTelasMultiplas1/app_telas_multiplas_1/android/app/src/main/kotlin/apptelasmultiplastextcom/example/app_telas_multiplas_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package apptelasmultiplastextcom.example.app_telas_multiplas_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ AppTelasMultiplas1/app_telas_multiplas_1/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:app_telas_multiplas_1/tela2.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // runAPP função que controi a tela do app
+  // MaterialAPP função widget para Android
+  // home parametro inicial para chamar a tela
+  runApp(MaterialApp(
+    home: Telaprincipal(),
+  ));
+
+}
+
+class Telaprincipal extends StatelessWidget {
+  const Telaprincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aplicativo aula 03")
+    ),
+    body:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(color: Colors.blue, width: 400, height: 180,
+          child: Text("Tela 1", style: TextStyle(fontSize: 25),),),
+          // child é a relação com 1 widget
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Telasecundaria(),));
+          }, child: Text("Tela 2")),
+        ],
+        
+      ),
+    );
+  }
+}
