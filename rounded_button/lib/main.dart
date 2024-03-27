6d7bc0476035b328d9f45ea3cc5029927ddf76c6
Vinicius Cavalieri <125322132+ViniBOYs@users.noreply.github.com>
2024-03-26 23:05:44 -0300

    Colors Update Commit

--- rounded_button/lib/main.dart
+++ rounded_button/lib/main.dart
@@ -16,55 +16,61 @@ class Home extends StatelessWidget {
           title: Text("App aula 08 exemplo"),
           backgroundColor: Colors.cyan,
         ),
-        body: Container(
-          width:500,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                const Color.fromARGB(255, 4, 96, 172),
-                Colors.blue,
-              ]
-            )
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-        
-              Container(width: 300,height: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: const LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.blue,
-                  const Color.fromARGB(255, 4, 96, 172),
-                ]),
-                
+        body:        
+          Container(
+            // height: 200,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage("https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAxL3Jhd3BpeGVsb2ZmaWNlM19waG90b19vZl9ibGFja19nYWxheHlfc2ltcGxlX2JsYWNrX2JhY2tncm91bmRfYjQ5ODc5NTUtYjBiYy00ZjU3LTgyNDMtN2QyOTY3NWJkMmFkXzEuanBn.jpg"),
+                    fit: BoxFit.cover),
               ),
-              child: Column(
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(labelText: "Digite seu nome"),
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: "Digite sua senha"),
-                    obscureText: true,
-                    // obscuringCharacter: '#', // modifica o que vai esconder os caracteres
-                  ),
-                  ElevatedButton(onPressed: (){}, child: Icon(Icons.account_circle_rounded)),
-                 
-                ],
-              ),
-              )
-            ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 300,height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(110, 33, 149, 243),
+                    Color.fromARGB(40, 4, 96, 172),
+                  ]),
+                  
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        labelText: "Digite seu nome",
+                        labelStyle: TextStyle(
+                          color: Colors.white, 
+                          fontWeight: FontWeight.bold,
+                          fontSize: 23
+                          ),
+                        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white,))
+                        ),
+                      style: TextStyle(color: Colors.amber)
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: "Digite sua senha"),
+                      obscureText: true,
+                      // obscuringCharacter: '#', // modifica o que vai esconder os caracteres
+                    ),
+                    ElevatedButton(onPressed: (){}, child: Icon(Icons.account_circle_rounded)),
+                   
+                  ],
+                ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
